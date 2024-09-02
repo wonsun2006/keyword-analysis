8624c5f87e81c2331ce38861ed2d7bcfbc375ddd
wonsun2006 <wonsun2006@gmail.com>
2024-09-02 21:34:13 +0900

    Initial Commit

---
+++ keyword-analyzer/src/main/java/com/example/keywordanalyzer/KeywordAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.keywordanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeywordAnalyzerApplication.class, args);
+	}
+
+}
