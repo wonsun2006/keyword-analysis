1d648ca10ab4cb1a7007a9174d46d5ac16e92822
wonsun2006 <wonsun2006@gmail.com>
2024-09-05 15:18:17 +0900

    test: JPA 접속 테스트 수정
    Profile 활성화 어노테이션 제거
    - JPA를 사용하지 않는 환경

--- keyword-analyzer/src/test/java/com/example/keywordanalyzer/config/JpaConfigurationTest.java
+++ keyword-analyzer/src/test/java/com/example/keywordanalyzer/config/JpaConfigurationTest.java
@@ -5,11 +5,9 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import jakarta.persistence.EntityManager;
 
-@ActiveProfiles("jpa")
 @SpringBootTest
 public class JpaConfigurationTest {
 
