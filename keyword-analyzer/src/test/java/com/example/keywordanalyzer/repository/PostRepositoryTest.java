b9905a82d154ddf66297c2b5309be2b9f41dce1d
wonsun2006 <wonsun2006@gmail.com>
2024-09-06 17:50:39 +0900

    refactor: Repository 테스트 추상화
    BasicRepositoryTest 추가
    WordCollectionRepositoryTest, PostRepositoryTest 상속 설정

--- keyword-analyzer/src/test/java/com/example/keywordanalyzer/repository/PostRepositoryTest.java
+++ keyword-analyzer/src/test/java/com/example/keywordanalyzer/repository/PostRepositoryTest.java
@@ -1,23 +1,27 @@
 package com.example.keywordanalyzer.repository;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.keywordanalyzer.model.entity.Post;
 
 @SpringBootTest
-public class PostRepositoryTest {
+public class PostRepositoryTest extends BasicRepositoryTest<Post> {
 	@Autowired
-	private PostRepository postCollectionRepository;
+	public PostRepositoryTest(PostRepository repository) {
+		super(repository);
+	}
 
-	private Post createMockData() {
+	@Override
+	protected Long getId(Post entity) {
+		return entity.getId();
+	}
+
+	protected Post createMockData() {
 		String content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 		LocalDateTime createdAt = LocalDateTime.of(2024, 1, 1, 0, 0, 0);
 		Long collectionId = 1L;
@@ -25,57 +29,11 @@ public class PostRepositoryTest {
 		return new Post(content, createdAt, collectionId, apiId);
 	}
 
-	private void assertPostEquals(Post expected, Post actual) {
+	@Override
+	protected void assertDataEquals(Post expected, Post actual) {
 		assertEquals(expected.getContent(), actual.getContent());
 		assertEquals(expected.getCreatedAt(), actual.getCreatedAt());
 		assertEquals(expected.getCollectionId(), actual.getCollectionId());
 		assertEquals(expected.getApiId(), actual.getApiId());
-	}
-
-	@Test
-	void postCollectionRepositoryLoads() {
-		assertThat(postCollectionRepository).isNotNull();
-	}
-
-	@Test
-	void savePost() {
-		// Arrange
-		Post post = createMockData();
-
-		// Act
-		Post savedPost = postCollectionRepository.save(post);
-
-		// Assert
-		assertNotNull(savedPost);
-		assertPostEquals(post, savedPost);
-	}
-
-	@Test
-	void findPost() {
-		// Arrange
-		Post post = createMockData();
-		Post savedPost = postCollectionRepository.save(post);
-
-		// Act
-		Optional<Post> foundPost = postCollectionRepository.findById(savedPost.getId());
-
-		// Assert
-		assertTrue(foundPost.isPresent());
-		assertEquals(savedPost.getId(), foundPost.get().getId());
-		assertPostEquals(post, savedPost);
-	}
-
-	@Test
-	void deletePost() {
-		// Arrange
-		Post post = createMockData();
-		Post savedPost = postCollectionRepository.save(post);
-
-		// Act
-		postCollectionRepository.deleteById(savedPost.getId());
-
-		// Assert
-		Optional<Post> foundPost = postCollectionRepository.findById(savedPost.getId());
-		assertFalse(foundPost.isPresent());
 	}
 }
--- keyword-analyzer/src/test/java/com/example/keywordanalyzer/repository/WordCollectionRepositoryTest.java
+++ keyword-analyzer/src/test/java/com/example/keywordanalyzer/repository/WordCollectionRepositoryTest.java
@@ -1,10 +1,8 @@
 package com.example.keywordanalyzer.repository;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,23 +13,29 @@ import com.example.keywordanalyzer.model.constants.CollectStatus;
 import com.example.keywordanalyzer.model.entity.WordCollection;
 
 @SpringBootTest
-public class WordCollectionRepositoryTest {
+public class WordCollectionRepositoryTest extends BasicRepositoryTest<WordCollection> {
 	@Autowired
-	private WordCollectionRepository wordCollectionRepository;
+	public WordCollectionRepositoryTest(WordCollectionRepository repository) {
+		super(repository);
+	}
 
-	private WordCollection createMockData() {
+	@Override
+	protected Long getId(WordCollection entity) {
+		return entity.getId();
+	}
+
+	protected WordCollection createMockData() {
 		int collectStatus = CollectStatus.NOT_STARTED;
 		int analysisStatus = AnalysisStatus.NOT_STARTED;
 		LocalDateTime startTime = LocalDateTime.of(2024, 1, 1, 0, 0, 0);
 		LocalDateTime endTime = LocalDateTime.of(2024, 1, 1, 1, 0, 0);
 		int totalDocCount = 0;
 		String message = "test";
-		return new WordCollection(collectStatus, analysisStatus, startTime, endTime,
-			totalDocCount,
-			message);
+		return new WordCollection(collectStatus, analysisStatus, startTime, endTime, totalDocCount, message);
 	}
 
-	private void assertWordCollectionEquals(WordCollection expected, WordCollection actual) {
+	@Override
+	protected void assertDataEquals(WordCollection expected, WordCollection actual) {
 		assertEquals(expected.getCollectStatus(), actual.getCollectStatus());
 		assertEquals(expected.getAnalysisStatus(), actual.getAnalysisStatus());
 		assertEquals(expected.getStartTime(), actual.getStartTime());
@@ -41,77 +45,30 @@ public class WordCollectionRepositoryTest {
 	}
 
 	@Test
-	void wordCollectionRepositoryLoads() {
-		assertThat(wordCollectionRepository).isNotNull();
-	}
-
-	@Test
-	void saveWordCollection() {
-		// Arrange
-		WordCollection collection = createMockData();
-
-		// Act
-		WordCollection savedWordCollection = wordCollectionRepository.save(collection);
-
-		// Assert
-		assertNotNull(savedWordCollection);
-		assertWordCollectionEquals(collection, savedWordCollection);
-	}
-
-	@Test
-	void findWordCollection() {
-		// Arrange
-		WordCollection collection = createMockData();
-		WordCollection savedWordCollection = wordCollectionRepository.save(collection);
-
-		// Act
-		Optional<WordCollection> foundWordCollection = wordCollectionRepository.findById(savedWordCollection.getId());
-
-		// Assert
-		assertTrue(foundWordCollection.isPresent());
-		assertEquals(savedWordCollection.getId(), foundWordCollection.get().getId());
-		assertWordCollectionEquals(collection, savedWordCollection);
-	}
-
-	@Test
 	void updateWordCollection() {
 		// Arrange
 		WordCollection collection = createMockData();
-		WordCollection savedWordCollection = wordCollectionRepository.save(collection);
+		WordCollection savedWordCollection = repository.save(collection);
 		int newCollectStatus = CollectStatus.IN_PROGRESS;
 		int newAnalysisStatus = AnalysisStatus.IN_PROGRESS;
 		LocalDateTime newStartTime = LocalDateTime.of(2024, 1, 1, 0, 0, 0);
-		LocalDateTime new_endTime = LocalDateTime.of(2024, 1, 1, 2, 0, 0);
+		LocalDateTime newEndTime = LocalDateTime.of(2024, 1, 1, 2, 0, 0);
 		int newTotalDocCount = 1;
 		String newMessage = "test2";
 		WordCollection newCollection = new WordCollection(newCollectStatus, newAnalysisStatus, newStartTime,
-			new_endTime, newTotalDocCount, newMessage);
+			newEndTime, newTotalDocCount, newMessage);
 
 		// Act
 		savedWordCollection.setCollectStatus(newCollectStatus);
 		savedWordCollection.setAnalysisStatus(newAnalysisStatus);
-		savedWordCollection.setEndTime(new_endTime);
+		savedWordCollection.setEndTime(newEndTime);
 		savedWordCollection.setTotalDocCount(newTotalDocCount);
 		savedWordCollection.setMessage(newMessage);
-		WordCollection updatedWordCollection = wordCollectionRepository.save(savedWordCollection);
+		WordCollection updatedWordCollection = repository.save(savedWordCollection);
 
 		// Assert
 		assertNotNull(updatedWordCollection);
 		assertEquals(savedWordCollection.getId(), updatedWordCollection.getId());
-		assertWordCollectionEquals(newCollection, updatedWordCollection);
-	}
-
-	@Test
-	void deleteWordCollection() {
-		// Arrange
-		WordCollection collection = createMockData();
-		WordCollection savedWordCollection = wordCollectionRepository.save(collection);
-
-		// Act
-		wordCollectionRepository.deleteById(savedWordCollection.getId());
-
-		// Assert
-		Optional<WordCollection> foundWordCollection = wordCollectionRepository.findById(savedWordCollection.getId());
-		assertFalse(foundWordCollection.isPresent());
+		assertDataEquals(newCollection, updatedWordCollection);
 	}
 }
