5c37c29c615d60fb58b26475aeb4170d21fc5102
wonsun2006 <wonsun2006@gmail.com>
2024-09-09 14:45:30 +0900

    feat: TermRank 추가
    TermRank 레포지터리 테스트 코드 작성
    TermRank entity 추가
    TermRankRepository 추가

---
+++ keyword-analyzer/src/test/java/com/example/keywordanalyzer/repository/TermRankRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.example.keywordanalyzer.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.keywordanalyzer.model.entity.TermRank;
+
+@SpringBootTest
+public class TermRankRepositoryTest extends BasicRepositoryTest<TermRank> {
+	@Autowired
+	public TermRankRepositoryTest(TermRankRepository repository) {
+		super(repository);
+	}
+
+	@Override
+	protected Long getId(TermRank entity) {
+		return entity.getId();
+	}
+
+	@Override
+	protected TermRank createMockData() {
+		String term = "Lorem";
+		int rankValue = 1;
+		Long collectionId = 1L;
+		return new TermRank(term, rankValue, collectionId);
+	}
+
+	@Override
+	protected void assertDataEquals(TermRank expected, TermRank actual) {
+		assertEquals(expected.getTerm(), actual.getTerm());
+		assertEquals(expected.getRankValue(), actual.getRankValue());
+		assertEquals(expected.getCollectionId(), actual.getCollectionId());
+	}
+}
